cc47749e7e8a56e785c9c5fe33319daa566dec02
Kevin Breslin <KovuTheHusky@users.noreply.github.com>
2016-03-12 09:37:03 -0500

    Add userscript

---
+++ inject.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Fix Steam Guides
+// @namespace    KovuTheHusky
+// @version      1.0.0
+// @description  try to take over the world!
+// @author       KovuTheHusky
+// @match        http://steamcommunity.com/app/*/guides
+// @match        http://steamcommunity.com/app/*/guides/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/KovuTheHusky/Fix-Steam-Guides/master/inject.user.js
+// ==/UserScript==
+
+'use strict';
+
+var items = document.getElementsByClassName('workshopItemCollection');
+
+for (var i = 0; i < items.length; ++i) {
+    var element = items.item(i);
+    var link = element.getAttribute('onclick');
+    
+    var parent = element.parentNode;
+    var wrapper = document.createElement('a');
+    
+    wrapper.setAttribute('href', link.substring(19, link.length - 1));
+    element.removeAttribute('onclick');
+    
+    parent.replaceChild(wrapper, element);
+    wrapper.appendChild(element);
+}
