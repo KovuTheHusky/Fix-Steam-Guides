fec94dd55b304da58031b80b7f11699b5f29014c
Kevin Breslin <KovuTheHusky@users.noreply.github.com>
2016-03-12 10:49:40 -0500

    Change match pattern

--- inject.user.js
+++ inject.user.js
@@ -4,8 +4,7 @@
 // @version      1.0.0
 // @description  Replace onclick attributes with actual links on Steam pages.
 // @author       KovuTheHusky
-// @match        http://steamcommunity.com/app/*/guides
-// @match        http://steamcommunity.com/app/*/guides/*
+// @match        http://steamcommunity.com/app/*/guides*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/KovuTheHusky/Fix-Steam-Guides/master/inject.user.js
 // ==/UserScript==
@@ -17,13 +16,10 @@ var items = document.getElementsByClassName('workshopItemCollection');
 for (var i = 0; i < items.length; ++i) {
     var element = items.item(i);
     var link = element.getAttribute('onclick');
-    
     var parent = element.parentNode;
     var wrapper = document.createElement('a');
-    
-    wrapper.setAttribute('href', link.substring(19, link.length - 1));
     element.removeAttribute('onclick');
-    
+    wrapper.setAttribute('href', link.substring(19, link.length - 1));
     parent.replaceChild(wrapper, element);
     wrapper.appendChild(element);
 }
